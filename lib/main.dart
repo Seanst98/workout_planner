484e30a1e7d56f6c8e8d4c54db25999c17e4331e
Sean Stirling <sean.a.stirling@gmail.com>
2018-12-30 21:15:21 +0000

    List of days displays as cards with icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(ExercisesPage());
+}
 
-class MyApp extends StatelessWidget {
+class ExercisesPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ExercisesPageState();
+  }
+}
+
+class ExercisesPageState extends State<ExercisesPage> {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Workout Planner',
-      home: Exercises(),
-      theme: new ThemeData(
-        primaryColor: Colors.white,
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Workout Planner'),
+        ),
+        body: new Center(
+          child: new ExercisePageCycle(),
+        )
       ),
+    );
+  }
+}
+
+class ExercisePageCycle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        return new Column(
+          children: <Widget>[
+            new GestureDetector(
+
+              child: new Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    new ListTile(
+                      leading: Icon(Icons.adb),
+                      title: Text('Day: $i'),
+                      trailing: Icon(Icons.done_outline),
+                    )
+                  ],
+                ),
+              )
+            )
+          ]
+        );
+      }
     );
   }
 }
